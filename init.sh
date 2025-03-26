3f2afa45fb06a815c2fcdecc40cbafd52c64b21c
Armando Castillo H <140430929+armando-pm@users.noreply.github.com>
2025-03-26 09:55:05 -0600

    USER_HOME

--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Para descargar y ejecutar este script directamente con sudo:
+# wget -qO- https://raw.githubusercontent.com/armando-pm/init/refs/heads/main/init.sh | sudo bash
+
 # Asegurarse de que el script se ejecute como root
 if [ "$(id -u)" -ne 0 ]; then
     echo "Este script debe ejecutarse como root."
@@ -12,12 +15,15 @@ apt-get update -y
 # Instalar python3, pip, git, vim, unzip, curl sin preguntar
 apt-get install -y python3 python3-pip git vim unzip curl
 
+# Obtener el nombre del usuario que ejecuta el script
+USER_HOME=$(eval echo ~${SUDO_USER})
+
 # Crear el directorio .ssh si no existe
-mkdir -p ~/.ssh
-chmod 700 ~/.ssh
+mkdir -p "$USER_HOME/.ssh"
+chmod 700 "$USER_HOME/.ssh"
 
 # Agregar la llave al archivo authorized_keys
-echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINs2uc6hx1erSs+93cnTmZSMzYSwRScMQrtX2BM3mPyU admin" >> ~/.ssh/authorized_keys
-chmod 600 ~/.ssh/authorized_keys
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINs2uc6hx1erSs+93cnTmZSMzYSwRScMQrtX2BM3mPyU" >> "$USER_HOME/.ssh/authorized_keys"
+chmod 600 "$USER_HOME/.ssh/authorized_keys"
 
 echo "Script completado exitosamente."
